3ebd3b7d68429686e75509dc0cf32282b670fa35
I2S9 <111307883+I2S9@users.noreply.github.com>
2023-12-05 13:08:41 +0100

    Create mirror-tree.c

---
+++ trees/mirror-tree.c
@@ -0,0 +1,7 @@
+Algo arbre_miroir(a : Arbre)
+    Si (est_vide(a) ou taille(a) = 1) alors
+        renvoyer a 
+    Sinon 
+        renvoyer cons(racine(a),arbre_miroir(droit(a)), arbre_miroir(gauche(a)))
+    Finsi
+FinAlgo
